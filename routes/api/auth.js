1b8cae5c058777cf490c4ab7ba12e571cb469ea2
Khrystyna Halaiko <gayka1818@gmail.com>
2023-03-09 17:22:15 +0200

    add

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 const { schemas } = require('../../models/user');
 
-const {validateBody, authenticate} = require('../../middlewares')
+const {validateBody, authenticate, upload} = require('../../middlewares')
 
 const ctrl = require('../../controllers/auth');
 
@@ -16,6 +16,8 @@ router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 
 router.post("/logout", authenticate, ctrl.logout);
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 
 module.exports = router;
